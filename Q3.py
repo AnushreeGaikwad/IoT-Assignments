f6c29da81d47a87b46ca335b894e9b2a02960846
AnushreeGaikwad <anushreegaikwad27@gmail.com>
2024-05-02 13:23:04 -0400

    Day10_numpy

--- Q3.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Enter First number : "))
-b = int(input("Enter Second number : "))
-c = int(input("Enter Third number : "))
-average = (a + b + c) / 3
-print(f"The average of three numbers = {average}")
--- Q6.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    fact = 1
-    for i in range(1, n+1):
-        fact = fact * i
-    return fact
-
-n = int(input("Enter positive number 'N' : "))
-f = factorial(n)
-print('Factorial is : ',f)
